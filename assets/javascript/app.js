44a3f49d9e50517a40821634dfa5c3d693081eb0
Michael Anderson <MacTerrier0@michaels-mbp.attlocal.net>
2019-03-15 23:22:12 -0700

    updated

---
+++ assets/javascript/app.js
@@ -0,0 +1,57 @@
+// Set the question data for the quiz
+const questionData = [
+    {
+        question: '',
+        answers: {
+            A: '',
+            B: '',
+            C: ''
+        },
+        correctAnswer: ''
+    },
+    {
+        question: '',
+        answers: {
+            A: '',
+            B: '',
+            C: ''
+        },
+        correctAnswer: ''
+    },
+    {
+        question: '',
+        answers: {
+            A: '',
+            B: '',
+            C: ''
+        },
+        correctAnswer: ''
+    },
+    {
+        question: '',
+        answers: {
+            A: '',
+            B: '',
+            C: ''
+        },
+        correctAnswer: ''
+    },
+    {
+        question: '',
+        answers: {
+            A: '',
+            B: '',
+            C: ''
+        },
+        correctAnswer: ''
+    }
+]
+
+// layout the quiz
+const quizBuilder = _ => {}
+
+// show the results to the quiz
+const dispResults = _ => {}
+
+// add event listener to process the results
+
