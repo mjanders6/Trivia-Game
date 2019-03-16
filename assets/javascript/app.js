e4efe18fb059aac56c74b12cdce639589c26a59e
Michael Anderson <MacTerrier0@dhcp-v119-031.mobile.uci.edu>
2019-03-16 15:52:10 -0700

    added a forEach to the questions on an API

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,32 @@
+fetch(`https://opentdb.com/api.php?amount=10&category=20&difficulty=medium`)
+    .then(r => r.json())
+    .then(r => {
+        console.log(r.results[0].question);
+        
+        r.results.forEach(ques => {
+            let quesElem = document.createElement('h5')
+            quesElem.textContent = ques.question
+            document.querySelector('#quizInfo').append(quesElem)
+        })
+        // category: "Mythology"
+        // correct_answer: "Participating in cannibalism."
+        // difficulty: "medium"
+        // incorrect_answers: Array(3)
+        // 0: "Excessive mutilation of animal corpses."
+        // 1: "Performing a ritual involving murder."
+        // 2: "Drinking the blood of many slain animals."
+        // length: 3
+        // __proto__: Array(0)
+        // question: "According to Algonquian folklore, how does one transform into a Wendigo?"
+        // type: "multiple"
+        console.log(r);
+
+    })
+    .then(e => {
+        console.log(e);
+
+    })
+
 // Set the question data for the quiz
 const questionData = [
     {
